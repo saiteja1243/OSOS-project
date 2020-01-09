14d77fe9100472d041f413f8d64f46b5dae0d4b5
sai <saiteja1243@gmail.com>
2020-01-09 11:28:22 +0530

    add to repo 2

---
+++ user.js
@@ -0,0 +1,16 @@
+var mongoose=require('mongoose')
+
+var User = new mongoose.Schema({
+    name:{
+        type:String
+    },
+    Password:{
+        type:String
+    },
+    PhoneNumber:{
+        type:Number
+    }
+})
+
+
+module.exports = mongoose.model('User',User);
